d0c9f25be301e7364157fe84c9ae5393495b1025
Chris Valencerina <chrisvthedeveloper@gmail.com>
2018-10-17 21:34:40 -0700

    animations and responsiveness

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+// ScrollReveal({ reset: true });
+ScrollReveal().reveal('.about-us', { delay: 500 });
+ScrollReveal().reveal('.about-photo-gallery', { delay: 800 });
+ScrollReveal().reveal('.our-services', { delay: 1200 });
+ScrollReveal().reveal('.services-row1', { delay: 1200 });
+ScrollReveal().reveal('.services-row2', { delay: 1000 });
+ScrollReveal().reveal('.ceo-image', { delay: 800 });
+ScrollReveal().reveal('.ceo-text', { delay: 1500 });
+ScrollReveal().reveal('.team', { delay: 1000 });
+ScrollReveal().reveal('.team-pictures', { delay: 1200 });
+ScrollReveal().reveal('.testimonials', { delay: 1000 });
+ScrollReveal().reveal('.contact-us', { delay: 1000 });
+ScrollReveal().reveal('.footer');
+
 $(document).foundation()
