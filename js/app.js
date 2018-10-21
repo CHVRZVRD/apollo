06bca376e9ecc51a6c7aa23a63c8ca6adfbea3a5
Chris Valencerina <chrisvthedeveloper@gmail.com>
2018-10-21 12:29:52 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,15 +1,98 @@
 // ScrollReveal({ reset: true });
-ScrollReveal().reveal('.about-us', { delay: 500 });
-ScrollReveal().reveal('.about-photo-gallery', { delay: 800 });
-ScrollReveal().reveal('.our-services', { delay: 1200 });
-ScrollReveal().reveal('.services-row1', { delay: 1200 });
-ScrollReveal().reveal('.services-row2', { delay: 1000 });
-ScrollReveal().reveal('.ceo-image', { delay: 800 });
-ScrollReveal().reveal('.ceo-text', { delay: 1500 });
-ScrollReveal().reveal('.team', { delay: 1000 });
-ScrollReveal().reveal('.team-pictures', { delay: 1200 });
-ScrollReveal().reveal('.testimonials', { delay: 1000 });
-ScrollReveal().reveal('.contact-us', { delay: 1000 });
-ScrollReveal().reveal('.footer');
+// ScrollReveal().reveal('.about-us', { delay: 500 });
+// ScrollReveal().reveal('.about-photo-gallery', { delay: 800 });
+// ScrollReveal().reveal('.our-services', { delay: 1200 });
+// ScrollReveal().reveal('.services-row1', { delay: 1200 });
+// ScrollReveal().reveal('.services-row2', { delay: 1000 });
+// ScrollReveal().reveal('.ceo-image', { delay: 800 });
+// ScrollReveal().reveal('.ceo-text', { delay: 1500 });
+// ScrollReveal().reveal('.team', { delay: 1000 });
+// ScrollReveal().reveal('.team-pictures', { delay: 1200 });
+// ScrollReveal().reveal('.testimonials', { delay: 1000 });
+// ScrollReveal().reveal('.contact-us', { delay: 1000 });
+// ScrollReveal().reveal('.footer');
+
+ScrollReveal().reveal('.about-us', { 
+    viewFactor: 0.5,
+    duration: 1000,
+    mobile: false 
+});
+
+
+ScrollReveal().reveal('.about-photo-gallery', { 
+    viewFactor: 0.3,
+    delay:500,
+    duration: 1000,
+    mobile: false 
+ });
+
+
+ScrollReveal().reveal('.our-services', { 
+    viewFactor: 0.5,
+    duration: 1000,
+    mobile: false 
+});
+
+ScrollReveal().reveal('.services-row1', { 
+    // delay: 900,
+    viewFactor: 0.8,
+    distance: '100px',
+    duration: 1200,
+    mobile: false  
+ });
+
+ScrollReveal().reveal('.services-row2', { 
+    // delay: 900,
+    viewFactor: 0.8,
+    distance: '100px',
+    duration: 1200,
+    mobile: false  
+ });
+
+ScrollReveal().reveal('.ceo-image', { 
+    viewFactor: 0.5,
+    distance: '110px',
+    duration: 1000,
+    origin: 'left',
+    mobile: false 
+});
+
+ScrollReveal().reveal('.ceo-text', { 
+    delay: 1400,
+    viewFactor: 0.3,
+    distance: '110px',
+    duration: 1000,
+    origin: 'right',
+    mobile: false 
+ });
+
+ScrollReveal().reveal('.team', { 
+    viewFactor: 0.5,
+    duration: 1000,
+    mobile: false 
+});
+
+ScrollReveal().reveal('.team-pictures', { 
+    viewFactor: 0.5,
+    delay: 800,
+    distance: '110px',
+    duration: 1000,
+    mobile: false 
+ });
+
+ScrollReveal().reveal('.testimonials', { 
+    viewFactor: 0.5,
+    delay:500,
+    distance: '110px',
+    duration: 1000,
+    mobile: false 
+ });
+
+ScrollReveal().reveal('.contact-us', { 
+    viewFactor: 0.5,
+    mobile: false 
+ });
+
+// ScrollReveal().reveal('.footer');
 
 $(document).foundation()
