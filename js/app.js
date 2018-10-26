e4de5aca768e5faf3d6e083c615dda1cab318f24
Chris Valencerina <chrisvthedeveloper@gmail.com>
2018-10-25 17:37:17 -0700

    update

--- js/app.js
+++ js/app.js
@@ -94,44 +94,44 @@
 //  });
 
 ScrollReveal().reveal('.about-us', { 
-    viewFactor: 0.5,
-    duration: 1000,
+    viewFactor: 0.4,
+    duration: 900,
     mobile: false 
 });
 
 ScrollReveal().reveal('.about-photo-gallery', { 
-    viewFactor: 0.2,
-    delay:500,
+    viewFactor: 0.1,
+    delay:400,
     duration: 1000,
     mobile: false 
  });
 
 
 ScrollReveal().reveal('.our-services', { 
-    viewFactor: 0.5,
-    duration: 1000,
+    viewFactor: 0.4,
+    duration: 900,
     mobile: false 
 });
 
 ScrollReveal().reveal('.services-row1', { 
     // delay: 900,
-    viewFactor: 0.8,
+    viewFactor: 0.7,
     // distance: '100px',
-    duration: 1200,
+    duration: 1100,
     mobile: false  
  });
 
 ScrollReveal().reveal('.services-row2', { 
     // delay: 900,
-    viewFactor: 0.8,
+    viewFactor: 0.7,
     // distance: '100px',
-    duration: 1200,
+    duration: 1100,
     mobile: false  
  });
 
 ScrollReveal().reveal('.ceo-image', { 
     delay: 500,
-    viewFactor: 0.2,
+    viewFactor: 0.1,
     // distance: '110px',
     duration: 1000,
     // origin: 'left',
@@ -139,38 +139,38 @@ ScrollReveal().reveal('.ceo-image', {
 });
 
 ScrollReveal().reveal('.ceo-text', { 
-    delay: 1200,
-    viewFactor: 0.3,
+    delay: 800,
+    viewFactor: 0.2,
     // distance: '110px',
-    duration: 1000,
+    duration: 600,
     // origin: 'right',
     mobile: false 
  });
 
 ScrollReveal().reveal('.team', { 
-    viewFactor: 0.5,
-    duration: 1000,
+    viewFactor: 0.4,
+    duration: 900,
     mobile: false 
 });
 
 ScrollReveal().reveal('.team-pictures', { 
-    viewFactor: 0.5,
-    delay: 800,
+    viewFactor: 0.4,
+    delay: 700,
     // distance: '110px',
-    duration: 1000,
+    duration: 900,
     mobile: false 
  });
 
 ScrollReveal().reveal('.testimonials', { 
-    viewFactor: 0.5,
-    delay:500,
+    viewFactor: 0.4,
+    delay:400,
     // distance: '110px',
-    duration: 1000,
+    duration: 900,
     mobile: false 
  });
 
 ScrollReveal().reveal('.contact-us', { 
-    viewFactor: 0.5,
+    viewFactor: 0.4,
     mobile: false 
  });
 
